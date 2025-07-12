66310bea1b6b75cf17c86c7d1b60281f60df28cb
Seppel <seppelhuang@163.com>
2025-07-13 07:28:00 +0800

    i

--- src/main/java/cn/seppel/smarttool/SmartToolApplication.java
+++ src/main/java/cn/seppel/smarttool/SmartToolApplication.java
@@ -31,7 +31,7 @@ public class SmartToolApplication implements CommandLineRunner {
         transformerService.transform(inputPath, outputPath);
         System.out.println("输出结果文件：" + outputPath);
         try {
-            Thread.sleep(2000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
            Thread.currentThread().interrupt();
         }
